b751e8134fd6078e2eac2040d0e11466b1c13bfa
pyderator <oscarmike286@gmail.com>
2022-04-19 11:35:41 +0545

    added pass test cases

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ test("adds 1+2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("This will fail", () => {
-  expect(sum(1, 1)).toBe(3);
+test("This will now pass", () => {
+  expect(sum(1, 1)).toBe(2);
 });
 
-test("This will also fail", () => {
-  expect(sum(2, 3)).toBe(3);
+test("This will also pass", () => {
+  expect(sum(2, 3)).toBe(5);
 });
